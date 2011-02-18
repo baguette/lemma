731ba18658e03d293917fc89cc79ca710807de24
Cory <cmburget@ncsu.edu>
2011-02-17 20:11:15 -0500

    Work on Lemma's expression parser. Next stop: evaluation.

--- lemma.lua
+++ lemma.lua
@@ -2,11 +2,14 @@
 local symbol =          -- this is perhaps a little permissive
 [[^([%a%-%?%*%+%%%$%^<>/\_=~:&|!][%a%d%-%?%*%+%%%$%^<>/\_=~:&|!@']*)]]
 
+local stringp = '^(".-")'
+
 -- these are tried in undefined order (make specific!)
 local atoms = {
    ['^(%()']            = 'open',
    ['^(%))']            = 'close',
-   ['^(".-")']          = 'string', -- strings cannot be escaped yet
+   ['^(\\)']            = 'backslash',
+   [stringp]            = 'string', -- strings cannot be escaped yet
    ['^([%d%.]+)']       = 'number',
    ['^(#\\[^%s%(%)]+)'] = 'character',
    ['^(\')']            = 'quote',
@@ -14,7 +17,7 @@ local atoms = {
    ['^(,)']             = 'unquote',
    ['^(@)']             = 'splice',
    ['^(;.-)$']          = 'comment',
-   ['^(#|)']            = 'opencomment',
+   ['^(#|)']            = 'opencomment', -- comments cannot be nested yet
    ['^(|#)']            = 'closecomment',
    ['^(#;)']            = 'datumcomment',
    [symbol]             = 'symbol'
@@ -22,51 +25,102 @@ local atoms = {
 
 local whitespace = '^([%s\n]+)'
 
-function tokens(str)
+function expr(f)    -- read one expression from f, nil on error, 'eof' on eof
+   local parens = 0
+   local lines = 0
    local t = {}
-   local n
+   
+   repeat
+     local n = 1
+     local str = f:read()   -- get one line at a time
+     
+     if not str then
+        return 'eof', lines
+     end
+     
+     local len = string.len(str)
+     
+     lines = lines + 1
+     
+     -- should probably watch out for unmatched tokens...
+     -- (loops forever on trailing newline, probably other tokens too)
+     while n <= len do
+        local oldn = n
+      
+        local i, j, c = string.find(str, whitespace, n)   -- eat spaces
+        if c then n = j + 1 end
+      
+        for k, v in pairs(atoms) do
+           local i, j, c = string.find(str, k, n)
+           if c then
+              if v == 'open' then
+                 parens = parens + 1
+              elseif v == 'close' then
+                 parens = parens - 1
+              elseif v == 'number' then
+                 c = c + 0          -- coerce c to a number value
+              elseif v == 'string' then
+                 local m = j
+                 while string.sub(c, -2, -2) == '\\' do
+                    local _, d
+                    _, m, d = string.find(str, stringp, m)
+                    if d then
+                       c = c .. d
+                    else
+                       return nil, lines, 'error building string: '..c
+                    end
+                 end
+                 j = m
+                 c = string.sub(c, 2, -2)
+                 c = string.gsub(c, '\\""', '"')          
+              end
+              table.insert(t, {c, v})
+              n = j + 1
+           end
+        end
+      
+        if n == oldn then
+           return nil, lines, 'lexical error: '.. string.sub(str, n)
+        elseif parens < 0 then
+           return nil, lines, 'unmatched ) paren: '
+        end
+     end
+   until parens == 0
 
-   len = string.len(str)
-   n = 1
-   
-   -- should probably watch out for unmatched tokens...
-   -- (loops forever on trailing newline, probably other tokens too)
-   while n < len do
-      local oldn = n
-      
-      local i, j, c = string.find(str, whitespace, n)   -- eat spaces
-      if c then n = j + 1 end
-   
-      for k, v in pairs(atoms) do
-         local i, j, c = string.find(str, k, n)
-         if c then
-            print ('match:', c, v)
-            table.insert(t, c)    -- save {c, v} in here after testing is done
-            n = j + 1
-         end
-      end
-      
-      if n == oldn then
-         return nil, 'lexical error: '.. string.sub(str, n)
-      end
-   end
-   
-   return t
+   return t, lines
 end
 
+
 local fname = ...
-local f = io.open(fname, 'r')
-
+local f, prompt
+local done = false
 local count = 0
-for line in f:lines() do
-   local t, e = tokens(line)
-   count = count + 1
 
-   print "--- TEST ---"
+if fname then
+   f = io.open(fname, 'r')
+   prompt = nil
+else
+   f = io.input()
+   prompt = '> '
+end
+
+local exps = 0
+while not done do
+   if prompt then io.write(prompt) end
+   
+   local t, c, e = expr(f)
+   count = count + c
+   
    if t then
-      print (table.concat(t, ' '))
+      if t == 'eof' then
+         done = true
+      else
+         for i, v in ipairs(t) do
+            t[i] = table.concat(v, ':')
+         end
+         print (table.concat(t, ' '))
+      end
    else
       print (count .. ': ' .. e)
    end
-   print "------------"
 end
